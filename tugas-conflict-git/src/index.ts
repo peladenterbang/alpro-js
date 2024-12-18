23b9d64f00fae9f6ed0eb973a22e259124bf842c
Your Name <caturahmad.github@gmail.com>
2024-12-18 21:16:12 +0700

    add fitur user authentication

---
+++ tugas-conflict-git/src/index.ts
@@ -0,0 +1,32 @@
+import express, {Request, Response} from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// Endpoint untuk register user
+app.post('/register', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logika untuk register user
+    res.send('User registered');
+});
+
+// Endpoint untuk login user
+app.post('/login', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logika untuk login user
+    res.send('User logged in');
+});
+
+
+app.listen(3000, () => {
+    console.log(`server start on port ${port}`);
+})
+
+
+
