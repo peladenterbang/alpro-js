1d0f25ae796f881622dc92ced5baaf1b0c6dc798
Your Name <caturahmad.github@gmail.com>
2024-12-24 19:48:25 +0700

    add middleware

--- tugas-jwt/src/routes/api.ts
+++ tugas-jwt/src/routes/api.ts
@@ -4,10 +4,11 @@ const router = express.Router();
 
 import authController from "../controller/auth.controller";
 import authMiddleware from "../middleware/auth.middleware";
+import rbacMiddleware from "../middleware/rbac.middleware";
 
 router.post("/auth/login", authController.login);
 router.post("/auth/register", authController.register);
-router.get("/auth/me",authMiddleware, authController.me);
+router.get("/auth/me",[authMiddleware,rbacMiddleware("admin")], authController.me);
 router.put("/auth/update-profile", authMiddleware,authController.updateProfile);
 
 export default router;
